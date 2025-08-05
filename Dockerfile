eefe082960a12c488063927f998fedae4f037f0e
Trevor <ebyt@vcu.edu>
2025-08-05 16:41:50 -0400

    ollama changes

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,8 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+RUN apt update && apt install -y curl
+
 # Copy rest of the application code
 COPY . .
 
--- backend/get_embedding_function.py
+++ backend/get_embedding_function.py
@@ -2,4 +2,4 @@ from langchain_ollama.embeddings import OllamaEmbeddings
 
 
 def get_embedding_function():
-    return OllamaEmbeddings(model="mxbai-embed-large")
+    return OllamaEmbeddings(base_url="http://127.0.0.1:11434")
